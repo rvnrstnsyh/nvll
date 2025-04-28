e8d0c350c7d3e0ae7fbc4a3a2cf6ef7773fcdbaf
Rivane Rasetiansyah <re@nvll.me>
2025-04-28 17:52:15 +0700

    fix: database schema
    users

--- src/helpers/database/schemas/users/schema.ts
+++ src/helpers/database/schemas/users/schema.ts
@@ -1,7 +1,7 @@
-import { pgTable, serial, varchar } from 'drizzle-orm/pg-core'
+import { integer, pgTable, varchar } from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users', {
-	id: serial().primaryKey(),
-	name: varchar({ length: 255 }).notNull(),
-	email: varchar({ length: 255 }).notNull().unique(),
+	id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+	name: varchar('name', { length: 255 }).notNull(),
+	email: varchar('email', { length: 255 }).notNull().unique(),
 })
